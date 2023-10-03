a34f30ae5650894e36970054082462985c69d672
HOME <kraynoff.i@yandex.ru>
2023-10-03 15:18:03 +0300

    logger to time save added

---
+++ LOB/save_process.py
@@ -0,0 +1,9 @@
+import time
+import datetime
+import os
+
+while True:
+    os.system('cp -av "/content/drive/MyDrive/LOB/Temp/callbacks" "/content/drive/MyDrive/LOB/Save"')
+    with open('save_process.log','a')as file:
+        file.write(f'Copied at {datetime.datetime.now().strftime("%H:%M:%S %d.%m")}\n')
+    time.sleep(120)
